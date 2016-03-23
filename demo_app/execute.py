18b455617c8721cdd29f47fe0853cbfed1f66a4b
sushant-kumar <sshnt.kmr3@gmail.com>
2016-03-23 21:12:31 +0530

    Handled streaming and auto-completion of filenames

--- demo_app/execute.py
+++ demo_app/execute.py
@@ -1,12 +1,23 @@
 import os
-
+import re
+#!/usr/bin/env python3
 def totem(command):
     cl = 'totem ' + command['intent']
     if command['intent'] == '--play':
         if command['arguments']['name']:
+            command['arguments']['name'] = command['arguments']['name'].strip(' ')
             # mutiple filenames and stuff aren't supported yet
             # give the proper filename if you want to play something
-            cl += ' ' + command['arguments']['name']
+            for dirName, subdirList, fileList in os.walk("./"):
+                for filename in fileList:
+                    if (re.search(command['arguments']['name'], filename, re.IGNORECASE)):
+                        print(filename)
+                        cl += ' ' + '"' + filename + '"'                        
+                        break
+                        # li.append(filename)
+            # cl += ' ' + command['arguments']['name']
+    cl += ' &'
+    print(cl)
     return_value = os.system(cl)
     if return_value == 0:
         return {
--- demo_app/static/js/main.js
+++ demo_app/static/js/main.js
@@ -1,9 +1,43 @@
 $(document).ready(function() {
-  
+    // var recorder = [undefined];
+    var streaming = new webkitSpeechRecognition();
+    streaming.start();
+    var s = function () {
+      // recorder[0] = streaming;
+      streaming.lang = 'en-IN';
+      streaming.continuous = true;
+      streaming.interimResults = false;    
+
+      streaming.onresult = function(event) {
+        var transcription_textContent = "";
+        for (var i = event.resultIndex; i < event.results.length; i++) {
+            transcription_textContent += event.results[i][0].transcript;
+          }
+        transcription_textContent = transcription_textContent.toLowerCase();
+        console.log(transcription_textContent);
+        $('input[name=command_text]').val(transcription_textContent);
+        var length = transcription_textContent.length;
+        var pos_listen = transcription_textContent.search("listen");
+
+        if (pos_listen != -1) {
+          var command_exe = transcription_textContent.substring(pos_listen+7);
+          console.log(command_exe);
+          $('input[name=command_text]').val(command_exe);
+          $("#main-submit").click();
+        }
+      }
+
+      streaming.onend = function(event) {
+        streaming.start();
+      }   
+    }
+
+    s();
   // This will be executed when the page is loaded
   (function() {
-    $('#result').parent().hide()
-  })()
+    $('#result').parent().hide();
+    // s();
+  })()   
 
   // Handles voice input
   $('#main-speech').click(function() {
@@ -28,6 +62,8 @@ $(document).ready(function() {
   // Submits form using AJAX
   $('#main-submit').click(function(e) {
     e.preventDefault()
+    // recorder[0].stop()
+    // streaming.stop();
     var submit = function() {
       var command = $('input[name=command_text]').val()
       // console.log(command)
@@ -40,11 +76,19 @@ $(document).ready(function() {
         success: function(result) {
           var output_string = JSON.stringify(result, null, 2)
           $('#result').html(output_string).show().parent().show()
-          console.log(output_string)
+          // console.log(output_string)
+          streaming.stop();
+          s();
         },
         error: function() {}
       })
     }
     submit()
   })
+
+  // $('#ss').on('click', function(event){
+  //   console.log(1)
+  //   $('input[name=command_text]').val("play song1.mp3");
+  //   $("#main-submit").click();
+  // })
 })
