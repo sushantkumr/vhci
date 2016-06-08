cd8e9385e7b69d099689f4138af0c750944aca50
rohithpr <praroh2@gmail.com>
2016-06-08 19:53:37 +0530

    Added event details

--- demo_app/static/js/recognizer.js
+++ demo_app/static/js/recognizer.js
@@ -9,10 +9,17 @@ var setupRecognizer = function () {
   }
   recognizer = new webkitSpeechRecognition() // Create an instance of class
   recognizer.lang = 'en-IN' // Set language to English India
-  recognizer.interimResults = false
+  recognizer.interimResults = false // We want results only after the user stops talking and not in-between
 
   // Function binding for onresult
+  // Event.results is an array in which different parts of the sentence will be stored
+  // It has only one element [0] as we have disabled interimResults
+  // [0][0] because we have only one possible sentence. Will have [0][1], [0][2] etc if we get more than possibility eg star, start etc. We're not using that.
+  // That is an object with the properties transcript and confidence
+  // confidence has the probability that the transcript is exactly what the user spoke
+  // transcript is what the user has spoken
   recognizer.onresult = function(event) {
+    // Convert transcript to lower case and remove spaces from wither end
     var inputContent = event.results[0][0].transcript.toLowerCase().trim()
     console.log('inputContent: ', inputContent)
 
