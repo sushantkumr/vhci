595d3bfc083f389bd8e40bf63be6b3beb5ecbed8
rohithpr <praroh2@gmail.com>
2016-06-09 00:49:24 +0530

    utils.py

--- ttcc/utils.py
+++ ttcc/utils.py
@@ -1,10 +1,6 @@
 def text2int(textnum, numwords={}):
-    '''
-    ('thirty five') -> 35
-    ('first option') -> 1
-    ('random text') -> None
-    '''
     # If the input is already a number we don't have to go through this process
+    # Eg: text2int('34') -> 34
     try:
         return int(textnum)
     except:
@@ -22,6 +18,10 @@ def text2int(textnum, numwords={}):
         scales = ["hundred", "thousand", "million", "billion", "trillion"]
 
         numwords["and"] = (1, 0)
+        # (idx, word) like (0, "zero"), (2, "twenty") etc
+        # numwords["zero"] = (1, 0)
+        # numwords["twenty"] = (1, 20)
+        # numwords["hundred"] = (1, 100)
         for idx, word in enumerate(units): numwords[word] = (1, idx)
         for idx, word in enumerate(tens): numwords[word] = (1, idx * 10)
         for idx, word in enumerate(scales): numwords[word] = (10 ** (idx * 3 or 2), 0)
@@ -33,25 +33,27 @@ def text2int(textnum, numwords={}):
 
     current = result = 0
     for word in textnum.split():
-        if word in ordinal_words:
+        if word in ordinal_words: # first, second etc
             scale, increment = (1, ordinal_words[word])
         else:
-            for ending, replacement in ordinal_endings:
+            for ending, replacement in ordinal_endings: # tenth -> ten, twentieth -> twenty
                 if word.endswith(ending):
                     word = "%s%s" % (word[:-len(ending)], replacement)
 
-            if word not in numwords:
+            if word not in numwords: # Ignore words like "option" etc
                 continue
 
             scale, increment = numwords[word]
 
         current = current * scale + increment
-        if scale > 100:
+        if scale > 100: # If thousand etc, store value and continue from 0
             result += current
             current = 0
 
     number = result + current
 
+    # If "hello world" return None
+    # If "zero" return 0
     if number == 0 and 'zero' not in textnum:
         return None
 
